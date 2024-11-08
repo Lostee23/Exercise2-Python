296285fff760b7b6e435c3812ab86fcb5e00a706
Lostee24 <lostilesphelele@gmail.com>
2024-11-08 11:38:33 +0200

    :tada:

---
+++ Exercise2.py
@@ -0,0 +1,80 @@
+# Question 1: Arithmetic and Assignment Operators
+x=9
+y=3
+# TODO: Add 3 to x using the += operator
+x +=3
+print("x")
+# TODO: Multiply y by 2 using the *= operator
+y *=2
+print("y")
+# TODO: Divide x by y and store the result in a variable called 'result'
+result= x/y 
+# TODO: Print the value of 'result'
+print(result)
+#------------------------------------------------------------------------------------
+# Question 2: Comparison and Logical Operators
+a = 2
+b = 4
+c = 6
+# TODO: Create a condition that checks if a is greater than b
+if a > b:
+# TODO: Create a condition that checks if b is even (hint: use the modulus operator)
+ modulus= b%2
+print(modulus) 
+# TODO: Create a condition that checks if c is less than or equal to a
+print(c<=a)
+# TODO: Combine the above conditions using logical operators to create a 'final_condition'
+#       The 'final_condition' should be True if either:
+#       - a is greater than b, or
+#       - b is even and c is less than or equal to a
+final_condition=(a>b) or ("%b") and (c<=a)
+# TODO: Print the value of 'final_condition'
+print("final_condition")
+#------------------------------------------------------------------------------------
+# Question 3: Conditional Statements
+
+# TODO: Ask the user to input a test score (0-100) and store it in a variable called 'score'
+score=input("please enter score")
+# TODO: Implement a grading system using if-elif-else statements:
+#       90-100: A
+#       80-89: B
+#       70-79: C
+#       60-69: D
+#       Below 60: F
+if score >=90:
+    print( "A" )
+elif score >=80:
+    print( "B" )
+elif score >=70:
+    print( "C" )
+elif score >=60:
+    print( "D" )
+else:
+    print( "F" )
+
+# TODO: Print the grade for the given score
+print(score)
+
+#------------------------------------------------------------------------------------
+# Question 4: Combining Operators and Conditionals
+
+# TODO: Ask the user to input two numbers and store them in variables 'num1' and 'num2'
+num1 = int(input("No.1"))
+num2 = int(("No.2"))
+# TODO: Ask the user to input an operation (+, -, *, /) and store it in a variable called 'operation'
+operation = input("Please choose an operation on num1 and num2")
+# TODO: Use conditional statements to perform the chosen operation on num1 and num2
+if operation == "+":
+    print(num1*num2)
+elif operation =="*":
+    print(num1*num2)
+elif operation == "-":
+    print(num1-num2)
+elif operation == "/":
+    print(num1/num2)
+# TODO: Handle the case of division by zero
+else:
+    print('Please enter valid operation') 
+
+# TODO: Print the result of the operation
+print(result)
